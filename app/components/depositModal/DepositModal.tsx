c85d9036e7c3fe9695aaaeed3dbf578db9ef2c85
Chris Manciero <cmanciero@gmail.com>
2025-03-16 18:50:08 -0400

    deposit modal

--- app/components/depositModal/DepositModal.tsx
+++ app/components/depositModal/DepositModal.tsx
@@ -23,67 +23,7 @@ function DepositModal({ type }: Props) {
 				className='border w-full p-2 max-w-xs outline-none'
 			/>
 			<div className='flex items-center w-full mt-4'>
-				<svg
-					v-if='fetching'
-					className='w-12 h-12'
-					version='1.1'
-					id='L4'
-					xmlns='http://www.w3.org/2000/svg'
-					xmlns:xlink='http://www.w3.org/1999/xlink'
-					x='0px'
-					y='0px'
-					viewBox='0 0 100 100'
-					enable-background='new 0 0 0 0'
-					xml:space='preserve'
-				>
-					<circle
-						fill='#4f329e'
-						stroke='none'
-						cx='6'
-						cy='50'
-						r='6'
-					>
-						<animate
-							attributeName='opacity'
-							dur='1s'
-							values='0;1;0'
-							repeatCount='indefinite'
-							begin='0.1'
-						/>
-					</circle>
-					<circle
-						fill='#4f329e'
-						stroke='none'
-						cx='26'
-						cy='50'
-						r='6'
-					>
-						<animate
-							attributeName='opacity'
-							dur='1s'
-							values='0;1;0'
-							repeatCount='indefinite'
-							begin='0.2'
-						/>
-					</circle>
-					<circle
-						fill='#4f329e'
-						stroke='none'
-						cx='46'
-						cy='50'
-						r='6'
-					>
-						<animate
-							attributeName='opacity'
-							dur='1s'
-							values='0;1;0'
-							repeatCount='indefinite'
-							begin='0.3'
-						/>
-					</circle>
-				</svg>
 				<button
-					v-else
 					onClick={handleClick}
 					className='hover:bg-purple-600 hover:text-white border bg-white px-4 border-purple-300 py-2 text-purple-600 uppercase font-semibold rounded cursor-pointer focus:outline-none'
 				>
